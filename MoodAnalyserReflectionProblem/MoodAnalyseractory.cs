12b1b12dae596f064b8d797d4b314f9cd58490c3
rasipriyanka <priyankapendela111@gmail.com>
2022-02-08 06:51:05 +0530

    [Priyanka]Added the implementation test case6.2

--- MoodAnalyserReflectionProblem/MoodAnalyseractory.cs
+++ MoodAnalyserReflectionProblem/MoodAnalyseractory.cs
@@ -5,17 +5,17 @@ using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-//-----namespace-------//
+
 namespace MoodAnalyserReflectionProblem
 {
-    public class MoodAnalyserFactory
+    public class MoodAnalyseractory
     {
         public static string CreateMoodAnalyse(string message, string methodName)
         {
             try
             {
                 Type type = Type.GetType("MoodAnalyserReflection.MoodAnalyser");
-                object moodAnalyserObject = MoodAnalyserFactory.CreateMoodAnalyse("MoodAnalyserReflection.MoodAnalyser", "MoodAnalyser");
+                object moodAnalyserObject = MoodAnalyseractory.CreateMoodAnalyse("MoodAnalyserReflection.MoodAnalyser", "MoodAnalyser");
                 MethodInfo method = type.GetMethod(methodName);
                 object mood = method.Invoke(moodAnalyserObject, null);
                 return mood.ToString();
@@ -23,13 +23,7 @@ namespace MoodAnalyserReflectionProblem
             catch (NullReferenceException)
             {
                 throw new MoodAnalyzerExcep(MoodAnalyzerExcep.ExceptionType.NO_SUCH_METHOD, "Method is not found");
-
             }
         }
     }
 }
-    
-
-
-
-
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -7,17 +7,32 @@ namespace TestProject1
     public class UnitTest1
     {
         [TestMethod]
-
         public void GiveMoodAnalyseClassNameShouldReturnMoodAnalyseObject()
         {
             //string message = "happy";
             //MoodAnalyser moodAnalyser = new MoodAnalyser(message);
             string exepected = "happy";
-            string obj = MoodAnalyserFactory.CreateMoodAnalyse("happy", "AnalyseMood");
+            string obj = MoodAnalyseractory.CreateMoodAnalyse("happy", "AnalyseMood");
             Assert.AreEqual(exepected, obj);
+        }
+        [TestMethod]
+        public void GivenImproperMethodToThrowCustomException() //this method for to catch the custom reflection
+                                                                //if the  name was different
+        {
+            string expected = "method is not found";
+            try
+            {
+                string obj = MoodAnalyseractory.CreateMoodAnalyse("happy", "AnalyseMood");
+            }
+            catch (MoodAnalyzerExcep ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
         }
     }
 }
+
+
 
 
 
